7d5c2b6ae7d08591bd29bd1bf98ef3da45596397
angelloantonnio <angelio123@hotmail.com>
2024-04-05 11:38:46 -0300

    fix: modificando rota principal para home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
         path: 'home',
         component: HomeComponent
     },
